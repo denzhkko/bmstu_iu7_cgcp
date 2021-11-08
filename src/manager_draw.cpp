bad9330f159b8550a17068aed1dd97967fbf0cf4
Nedoluzhko Denis <denisnedoluzhko1902@gmail.com>
2021-11-08 23:05:34 +0300

    hittable objects implemented

--- src/manager_draw.cpp
+++ src/manager_draw.cpp
@@ -4,33 +4,21 @@
 #include <iostream>  // cout
 #include <thread>    // thread
 
+#include "hittable_list.h"
 #include "ray.h"
+#include "rtweekend.h"
+#include "sphere.h"
 #include "vec3.h"
 
 using namespace std::literals::chrono_literals;
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  auto a = r.direction().length_squared();
-  auto half_b = dot(oc, r.direction());
-  auto c = oc.length_squared() - radius * radius;
-  auto discriminant = half_b * half_b - a * c;
-
-  if (discriminant < 0) {
-    return -1.0;
-  } else {
-    return (-half_b - sqrt(discriminant)) / a;
-  }
-}
-
-color ray_color(const ray& r) {
-  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-  if (t > 0.0) {
-    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world) {
+  hit_record rec;
+  if (world.hit(r, 0, infinity, rec)) {
+    return 0.5 * (rec.normal + color(1, 1, 1));
   }
   vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5 * (unit_direction.y() + 1.0);
+  auto t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -43,8 +31,15 @@ void manager_draw::draw(unsigned const width, unsigned const height,
         QImage image(img_w, img_h, QImage::Format::Format_ARGB32_Premultiplied);
         image.fill(QColor(255, 255, 255));
 
+        // Image
         const auto aspect_ratio = static_cast<double>(img_w) / img_h;
 
+        // World
+        hittable_list world;
+        world.add(std::make_shared<sphere>(point3{0, 0, -1}, 0.5));
+        world.add(std::make_shared<sphere>(point3{0, -100.5, -1}, 100));
+
+        // Camera
         auto viewport_height = 2.0;
         auto viewport_width = aspect_ratio * viewport_height;
         auto focal_length = 1.0;
@@ -61,7 +56,7 @@ void manager_draw::draw(unsigned const width, unsigned const height,
             auto v = double(j) / (img_h - 1);
             ray r(origin,
                   lower_left_corner + u * horizontal + v * vertical - origin);
-            color c = ray_color(r);
+            color c = ray_color(r, world);
 
             image.setPixelColor(i, j,
                                 {static_cast<int>(255.999 * c.x()),
@@ -75,6 +70,7 @@ void manager_draw::draw(unsigned const width, unsigned const height,
           if (is_cancelled()) break;
         }
 
+        image = image.mirrored(false, true);
         send_pic(image);
       },
       width, height);
